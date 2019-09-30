e9fce58f5c8aee6a2a2a6d3ca1b76eb54801c300
100134315 <100134315@lubbockisd.net>
2019-09-30 15:34:54 -0500

    playing with octothorpe

--- learning.py
+++ learning.py
@@ -1,3 +1,8 @@
+# what is a octothorpe?
+# <- this is an octothorpe
+# what is the octothorpe used for in python
+# notes, also known in the programing community as comments
+
 print("hello world.")
 print("howdy, y'all!")
 print("printing....yay!!!")
