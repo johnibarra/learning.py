2f068541b477c3cc3aa6ad1b499ccdafd45c5966
100134315 <100134315@lubbockisd.net>
2019-10-01 15:32:56 -0500

    playing with octothorpe i will now count chicks

--- learning.py
+++ learning.py
@@ -7,13 +7,66 @@ print("hello world.")
 print("howdy, y'all!")
 print("printing....yay!!!")
 
-
-
-
-
 # Practice with math and things
 print("I will now count my chickens.")
 print("hens", 25 + 30 / 6)
 print("roosters: ", 100 - 25 * 3 % 4)
 print("now, i will count the eggs:")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("i will now count the chicks.")
+print(5 + 3 + 4 - 10 + 24)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
