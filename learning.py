20bc426615d2c9aaad5db113265792653553b313
100134315 <100134315@lubbockisd.net>
2019-10-02 15:54:17 -0500

    i updated som numbers

--- learning.py
+++ learning.py
@@ -24,21 +24,22 @@ print(200.0 - 180.0)
 
 
 # variables
-cars = 100
+cars = 80
 space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
+drivers = 40
+passengers = 115
+CarsNotDriven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers/cars_driven
 
 print("there are",cars,"cars available.")
 print("there are only",drivers,"drivers available")
-print("there will be",cars_not_driven,"empty cars today")
+print("there will be", CarsNotDriven, "empty cars today")
 print("we can transport",carpool_capacity,"people today.")
 print("we have", passengers,"to carpool today.")
-print("")
+print("we need to put about", average_passengers_per_car,"in each car.")
+
 
 
 
